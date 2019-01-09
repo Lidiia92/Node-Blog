6c0d540dbc73cdff5a80b4fe538f27a0e94d150a
Lidii <lidiiany92@gmail.com>
2019-01-09 16:24:30 -0500

    Added /api/users endpoint for the get request

--- api/server.js
+++ api/server.js
@@ -12,6 +12,22 @@ const tagDb = require('../data/helpers/tagDb');
 
 
 //middleware - global
+server.use(express.json()); 
+server.use(helmet()); 
+server.use(morgan('short')); 
+server.use(cors()); 
 
+//routers
+server.get('/api/users', async (req, res, next) => {
+
+    try{
+        const userList = await userDb.get();
+        res.json(userList);
+    } 
+    catch (err) {
+        res.status(500).json({message: "Users can not be retrieved"})
+    }
+    
+});
 
 module.exports = server;
