5a24da7602f27cec93d20c57c14940174d056dd9
Lidii <lidiiany92@gmail.com>
2019-01-09 17:13:34 -0500

    Added upperCase function

--- api/server.js
+++ api/server.js
@@ -33,26 +33,25 @@ server.get('/api/users', async (req, res) => {
 function upperCase(req, res, next) {
     const user = req.body;
     const { name } = user;
-    const arr = name.split(" ");
-    const upperCased = arr.map(
-      item => item[0].toUpperCase() + item.slice(1).toLowerCase()
-    );
-    console.log("before", upperCased);
-    const joined = upperCased.join(" ");
-    console.log("bt2", upperCased);
-    res.send(joined);
+  
+    if (name) {
+      const arr = name.split(" ");
+      const upperCased = arr.map(
+        item => item[0].toUpperCase() + item.slice(1).toLowerCase()
+      );
+      const joined = upperCased.join(" ");
+      res.send(joined);
+    } else {
+      res.status(400).json({ message: "Please include user name" });
+    }
     next();
   }
 
 server.post('/api/users', upperCase, async (req, res) => {
     const user = req.body;
     try {
-      if (!user.name) {
-        res.status(400).json({ message: "Please include user name" });
-      } else {
         const result = await userDb.insert(user);
         res.status(201).json({ messege: `User ${user} has been created!` });
-      }
     } catch (err) {
       res
         .status(500)
