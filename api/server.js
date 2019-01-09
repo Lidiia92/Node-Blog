59ff36500bb07e6b4a7d3e480251f1111d94cd03
Lidii <lidiiany92@gmail.com>
2019-01-09 17:04:19 -0500

    Added upperCase function

--- api/server.js
+++ api/server.js
@@ -18,7 +18,7 @@ server.use(morgan('short'));
 server.use(cors()); 
 
 //routers
-server.get('/api/users', async (req, res, next) => {
+server.get('/api/users', async (req, res) => {
 
     try{
         const userList = await userDb.get();
@@ -28,6 +28,36 @@ server.get('/api/users', async (req, res, next) => {
         res.status(500).json({message: "Users can not be retrieved"})
     }
     
+});
+
+function upperCase(req, res, next) {
+    const user = req.body;
+    const { name } = user;
+    const arr = name.split(" ");
+    const upperCased = arr.map(
+      item => item[0].toUpperCase() + item.slice(1).toLowerCase()
+    );
+    console.log("before", upperCased);
+    const joined = upperCased.join(" ");
+    console.log("bt2", upperCased);
+    res.send(joined);
+    next();
+  }
+
+server.post('/api/users', upperCase, async (req, res) => {
+    const user = req.body;
+    try {
+      if (!user.name) {
+        res.status(400).json({ message: "Please include user name" });
+      } else {
+        const result = await userDb.insert(user);
+        res.status(201).json({ messege: `User ${user} has been created!` });
+      }
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "There was an error saving user to the database" });
+    }
 });
 
 module.exports = server;
